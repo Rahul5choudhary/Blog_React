ff281c00dfe78bb7d302623744a7f05ae769edc1
Rahul kumar <kumarrahulchoudhary91280@gmail.com>
2021-06-22 20:25:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import Navbar from './Navbar'
+import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 function App() {
+
+  // const title = "Welcome to the new blog";
+  // const likes = 50;
+  // const link = "http://www.google.com";
+  // const person ={name:'rahul',age:50};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <div className="content">
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+
+            <Route path="/blogs/:id">
+          <BlogDetails/>
+            </Route>
+
+
+            <Route path="*">
+          <NotFound/>
+            </Route>
+
+
+          </Switch>
+
+        </div>
+      </div>
+    </Router>
   );
 }
 
